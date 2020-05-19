bebf9ffa726cf126481dc779b88fe9808be1c417
KasiaKoz <kasia.kozlowska@arup.com>
2020-05-19 10:48:34 +0100

    init

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="genet",
+    version="0.0.1",
+    author="Kasia Kozlowska",
+    author_email="",
+    description="MATSim network scenario generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/arup-group/puma",
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+            'networkx',
+            's2sphere',
+            'coverage',
+            'pytest',
+            'pytest-mock'
+        ],
+)
