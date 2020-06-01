86a1070563440e6ae9636daad300100c49e8dd44
Alex Andrey <alexandreandrey402@gmail.com>
2020-06-01 18:02:18 +0100

    commenting out incomplete tests to avoid breaking the build

--- tests/test_road_pricing.py
+++ tests/test_road_pricing.py
@@ -39,43 +39,43 @@ def test_extract_toll_ways_from_opl(path_opl='tests/test_data/road_pricing/test.
 
 #########
 
-def test_extract_network_id_from_osm_id(n='test_data/road_pricing/???',
-                                        osm_way_ids='test_data/road_pricing/test_osm_toll_ids'):
-    toll_ids = road_pricing.extract_network_id_from_osm_id(n, osm_way_ids)
-    # check that returns a list
-    assert isinstance(toll_ids, list)
-    # check that the list is non-empty
-    assert len(toll_ids) > 0
-    # check that all items in list are strings
-    for item in toll_ids:
-        assert isinstance(item, str)
+# def test_extract_network_id_from_osm_id(n='test_data/road_pricing/???',
+#                                         osm_way_ids='test_data/road_pricing/test_osm_toll_ids'):
+#     toll_ids = road_pricing.extract_network_id_from_osm_id(n, osm_way_ids)
+#     # check that returns a list
+#     assert isinstance(toll_ids, list)
+#     # check that the list is non-empty
+#     assert len(toll_ids) > 0
+#     # check that all items in list are strings
+#     for item in toll_ids:
+#         assert isinstance(item, str)
 
-    # do we need to test whether the contents of `toll_ids` are indeed part of network `n` ?
-
-
-
-
-def test_write_xml(tmpdir):
-    root = Element("root", type="cordon", name="cordon-toll")
-    road_pricing.write_xml(root, tmpdir)
-
-    expected_xml = os.path.join(tmpdir, 'roadpricing-file.xml')
-    assert os.path.exists(expected_xml)
-
-    # parse xml and check:
-    # 1st line <?xml version="1.0" ?>
-    # 2nd line <!DOCTYPE roadpricing SYSTEM "http://matsim.org/files/dtd/roadpricing_v1.dtd"
-    # root cordon cordon-toll
+#     # do we need to test whether the contents of `toll_ids` are indeed part of network `n` ?
 
 
 
 
-def test_build_tree(network_toll_ids='test_data/road_pricing/test_network_ids',
-                    xml_schema_dtd='test_data/road_pricing/roadpricing_v1.dtd'):
-    root = road_pricing.build_tree(network_toll_ids)
+# def test_write_xml(tmpdir):
+#     root = Element("root", type="cordon", name="cordon-toll")
+#     road_pricing.write_xml(root, tmpdir)
 
-    # check type of root object
-    assert isinstance(root, ???)
-    # check DTD !!!
-    assert xml_schema_dtd
-    # do we need to check that links in `network_toll_ids` are present under <links> ?
+#     expected_xml = os.path.join(tmpdir, 'roadpricing-file.xml')
+#     assert os.path.exists(expected_xml)
+
+#     # parse xml and check:
+#     # 1st line <?xml version="1.0" ?>
+#     # 2nd line <!DOCTYPE roadpricing SYSTEM "http://matsim.org/files/dtd/roadpricing_v1.dtd"
+#     # root cordon cordon-toll
+
+
+
+
+# def test_build_tree(network_toll_ids='test_data/road_pricing/test_network_ids',
+#                     xml_schema_dtd='test_data/road_pricing/roadpricing_v1.dtd'):
+#     root = road_pricing.build_tree(network_toll_ids)
+
+#     # check type of root object
+#     assert isinstance(root, ???)
+#     # check DTD !!!
+#     assert xml_schema_dtd
+#     # do we need to check that links in `network_toll_ids` are present under <links> ?
