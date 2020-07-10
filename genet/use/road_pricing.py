f3657390e5a73447583001ef847922644206395c
Alex Andrey <alexandreandrey402@gmail.com>
2020-07-10 12:37:42 +0100

    using graph_operations to match osm ids to network ids

--- genet/use/road_pricing.py
+++ genet/use/road_pricing.py
@@ -1,49 +1,53 @@
 import os
+import json
 import pandas as pd
 from lxml import etree as et
 from lxml.etree import Element, SubElement, Comment
 from tqdm import tqdm
 
+from genet.utils import graph_operations
+
 
 # Below function should be deprecated - possibly replaced by an XML parser ?
-def extract_toll_ways_from_opl(path_opl):
-    '''
-    Given a .osm.opl file, extract the ids of toll ways
-    :param path_opl: path to the .osm.opl file
-    :return: list of OSM way ids matching criteria (str)
-    '''
-    # parse .osm.opl contents
-    with open(path_opl, 'r') as f:
-        contents = [line.rstrip('\n') for line in f]
-    # extract all OSM ways (lines starting with 'w')
-    ways = [line for line in contents if line[0] == 'w']
 
-    # extract toll ways from above OSM ways
-    toll_ways = []
-    with tqdm(total=len(ways)) as pbar:
-        for way in ways:
-            # below conditions are the result of iterations on Ireland OSM
-            # for another location, some unwanted ways could still persist
-            if all(['toll=yes' in way,
-                    'ferry' not in way,
-                    'highway=service' not in way,
-                    'highway=unclassified' not in way,
-                    'motor_vehicle=no' not in way,
-                    'access=private' not in way,
-                    'access=no' not in way]):
-                toll_ways.append(way)
-            pbar.update(1)
+# def extract_toll_ways_from_opl(path_opl):
+#     '''
+#     Given a .osm.opl file, extract the ids of toll ways
+#     :param path_opl: path to the .osm.opl file
+#     :return: list of OSM way ids matching criteria (str)
+#     '''
+#     # parse .osm.opl contents
+#     with open(path_opl, 'r') as f:
+#         contents = [line.rstrip('\n') for line in f]
+#     # extract all OSM ways (lines starting with 'w')
+#     ways = [line for line in contents if line[0] == 'w']
 
-    # extract ids from toll ways
-    toll_ids = [item.split(' ')[0] for item in toll_ways]
-    # summary for user
-    print('{} toll ways extracted from {}'.format(len(toll_ids), path_opl))
+#     # extract toll ways from above OSM ways
+#     toll_ways = []
+#     with tqdm(total=len(ways)) as pbar:
+#         for way in ways:
+#             # below conditions are the result of iterations on Ireland OSM
+#             # for another location, some unwanted ways could still persist
+#             if all(['toll=yes' in way,
+#                     'ferry' not in way,
+#                     'highway=service' not in way,
+#                     'highway=unclassified' not in way,
+#                     'motor_vehicle=no' not in way,
+#                     'access=private' not in way,
+#                     'access=no' not in way]):
+#                 toll_ways.append(way)
+#             pbar.update(1)
 
-    return toll_ids
+#     # extract ids from toll ways
+#     toll_ids = [item.split(' ')[0] for item in toll_ways]
+#     # summary for user
+#     print('{} toll ways extracted from {}'.format(len(toll_ids), path_opl))
 
-#  user should check that they are happy with links (osmium getid - QGIS - OSM layer)
-# hence why we provide write_toll_ids() and read_toll_ids(), as user may
-# add some ids manually
+#     return toll_ids
+
+# #  user should check that they are happy with links (osmium getid - QGIS - OSM layer)
+# # hence why we provide write_toll_ids() and read_toll_ids(), as user may
+# # add some ids manually
 
 
 def write_toll_ids(toll_ids, outpath):
@@ -118,58 +122,109 @@ def extract_network_id_from_osm_id(network, osm_way_ids):
     return network_toll_ids
 
 
-def extract_network_id_from_osm_csv(network, osm_csv_path, outpath):
+def extract_network_id_from_osm_csv(network, attribute_name, osm_csv_path, outpath):
     '''Parse a Network() object and find edges whose
-    ['attributes']['osm:way:id']['text'] is present in a list of OSM way ids
-    :param network: a Network() object with 'osm:way:id'
+    ['attributes'][attribute_name]['text'] is present in a list of OSM way ids
+    :param network: a Network() object with attribute_name tags
+    :param attribute_name: a string corresponding to the name of the link attribute of interest
     :param osm_csv_path: path to a .csv config file where OSM way ids are stored in column `osm_ids`
     :param outpath: path to a .csv file to be written
-    :return: None, but will write .csv file to `outpath` location
+    :return: None, but will write .csv and .json files to `outpath` location
     '''
+
     osm_df = pd.read_csv(osm_csv_path, dtype=str)
-    # for each row in osm_df, we want to add a values 'network_id'
     osm_df['network_id'] = pd.Series(dtype=str)
 
-    edge_osm_ids = set()
-    osm_ids = set(osm_df['osm_ids'].values)
+    target_osm_ids = set(osm_df['osm_ids'].values)
 
-    for link_id, link_attribs in network.links():
-        if 'attributes' in link_attribs.keys():
-            # extract the OSM id associated to that Network link
-            # edge_osm_id = link_attribs['attributes']['osm:way:id']['text']
-            edge_osm_id = link_attribs['attributes']['osm:osmid']['text']
-            if str(edge_osm_id) in osm_ids:
-                # if the OSM id associated to that Network link is one of the OSM toll ways we are interested in
-                # then we extract the indices of all rows in the .csv where that OSM toll way appears
-                temp_index = osm_df[osm_df['osm_ids'] == edge_osm_id].index
-                if len(temp_index) > 1:
-                    osm_df.loc[temp_index, 'network_id'] = link_id
-                else:
-                    temp_index = temp_index[0]
-                    osm_df.loc[temp_index, 'network_id'] = link_id
+    osm_to_network_dict = {}
 
-                edge_osm_ids.update(edge_osm_id)
-            else:
-                continue
+    for target_id in target_osm_ids:
+        links = graph_operations.extract_links_on_edge_attributes(
+                network,
+                conditions= {'attributes': {attribute_name: {'text': target_id}}},
+            )
 
-        edge_osm_ids = set(edge_osm_ids)
-        if edge_osm_ids == osm_ids:
-            # if we have found all the OSM way ids, no need to go through the rest of the Network
-            break
-        # else:
-        #     print(len(osm_ids) - len(edge_osm_ids), ' OSM ways still to be found.')
+        # links is now a list of strings
+        if len(links) > 0:
+            # store list of links in dictionary
+            osm_to_network_dict[target_id] = links
+            # mark the OSM id as "matched" in the dataframe
+            temp_index = osm_df[osm_df['osm_ids'] == target_id].index
+            osm_df.loc[temp_index, 'network_id'] = 'yes'
+        else:
+            # mark the OSM id as "ummatched" in the dataframe
+            temp_index = osm_df[osm_df['osm_ids'] == target_id].index
+            osm_df.loc[temp_index, 'network_id'] = 'no'
 
     # check whether some of our OSM ids were not found
-    unmatched_osm_df = osm_df[osm_df['network_id'].isnull()]
+    unmatched_osm_df = osm_df[osm_df['network_id']=='no']
     if unmatched_osm_df.shape[0] > 0:
         # print unmatched ids
         print('these OSM way ids did not find a match in the network.xml')
         print(unmatched_osm_df['osm_ids'].values)
-        # remove the corresponding rows from the dataframe
-        print('these OSM way ids have been removed from the output saved at '+outpath)
-        osm_df = osm_df.drop(osm_df[osm_df['network_id'].isnull()].index)
-    # write back to .csv
-    osm_df.to_csv(outpath, index=False)
+        # # remove the corresponding rows from the dataframe
+        # print('these OSM way ids have been removed from the output saved at '+outpath)
+        # osm_df = osm_df.drop(osm_df[osm_df['network_id'].isnull()].index)
+    
+    # write dataframe as .csv and dictionary as .json
+    osm_df.to_csv(os.path.join(outpath,'osm_tolls_with_network_ids.csv'), index=False)
+    with open(os.path.join(outpath,'osm_to_network_ids.json'), 'w') as write_file:
+        json.dump(osm_to_network_dict, write_file)
+
+
+# def extract_network_id_from_osm_csv(network, osm_csv_path, outpath):
+#     '''Parse a Network() object and find edges whose
+#     ['attributes']['osm:way:id']['text'] is present in a list of OSM way ids
+#     :param network: a Network() object with 'osm:way:id'
+#     :param osm_csv_path: path to a .csv config file where OSM way ids are stored in column `osm_ids`
+#     :param outpath: path to a .csv file to be written
+#     :return: None, but will write .csv file to `outpath` location
+#     '''
+#     osm_df = pd.read_csv(osm_csv_path, dtype=str)
+#     # for each row in osm_df, we want to add a values 'network_id'
+#     osm_df['network_id'] = pd.Series(dtype=str)
+
+#     edge_osm_ids = set()
+#     osm_ids = set(osm_df['osm_ids'].values)
+
+#     for link_id, link_attribs in network.links():
+#         if 'attributes' in link_attribs.keys():
+#             # extract the OSM id associated to that Network link
+#             # edge_osm_id = link_attribs['attributes']['osm:way:id']['text']
+#             edge_osm_id = link_attribs['attributes']['osm:osmid']['text']
+#             if str(edge_osm_id) in osm_ids:
+#                 # if the OSM id associated to that Network link is one of the OSM toll ways we are interested in
+#                 # then we extract the indices of all rows in the .csv where that OSM toll way appears
+#                 temp_index = osm_df[osm_df['osm_ids'] == edge_osm_id].index
+#                 if len(temp_index) > 1:
+#                     osm_df.loc[temp_index, 'network_id'] = link_id
+#                 else:
+#                     temp_index = temp_index[0]
+#                     osm_df.loc[temp_index, 'network_id'] = link_id
+
+#                 edge_osm_ids.update(edge_osm_id)
+#             else:
+#                 continue
+
+#         edge_osm_ids = set(edge_osm_ids)
+#         if edge_osm_ids == osm_ids:
+#             # if we have found all the OSM way ids, no need to go through the rest of the Network
+#             break
+#         # else:
+#         #     print(len(osm_ids) - len(edge_osm_ids), ' OSM ways still to be found.')
+
+#     # check whether some of our OSM ids were not found
+#     unmatched_osm_df = osm_df[osm_df['network_id'].isnull()]
+#     if unmatched_osm_df.shape[0] > 0:
+#         # print unmatched ids
+#         print('these OSM way ids did not find a match in the network.xml')
+#         print(unmatched_osm_df['osm_ids'].values)
+#         # remove the corresponding rows from the dataframe
+#         print('these OSM way ids have been removed from the output saved at '+outpath)
+#         osm_df = osm_df.drop(osm_df[osm_df['network_id'].isnull()].index)
+#     # write back to .csv
+#     osm_df.to_csv(outpath, index=False)
 
 
 def write_xml(root, path):
