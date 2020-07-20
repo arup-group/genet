11dedd24232acfec2fe32c9eec8acf9bbc31a0d1
Alex Andrey <alexandreandrey402@gmail.com>
2020-07-20 09:32:38 +0100

    cleaning up road pricing and tests

--- genet/use/road_pricing.py
+++ genet/use/road_pricing.py
@@ -48,76 +48,39 @@ from genet.utils import graph_operations
 # # add some ids manually
 
 
-def write_toll_ids(toll_ids, outpath):
-    '''
-    Given a list of way ids, write them to a file named 'toll_ids' where each is
-    on a separate line. Such files can be used to subset a .osm.pbf with osmium
-    e.g. `osmium getid -i toll_ids -r input.osm.pbf -o tolls.osm.pbf`
-    :param toll_ids: list of way ids (str)
-    :param outpath: path to destination folder
-    :return: write a list of way ids e.g. `w123456` (str)
-
-    the reason we are keeping the 'w' while writing the ids, is that they are
-    required by the osmium command detailed above
-    '''
-    with open(os.path.join(outpath, 'toll_ids'), 'w') as f:
-        for item in toll_ids:
-            temp = item
-            f.write("%s\n" % temp)
-
-
-def read_toll_ids(path):
-    '''
-    Read a list of OSM way ids
-    :param path: path to folder
-    :return: a list of OSM way ids (without the `w` prefix) (str)
-    '''
-    with open(path, 'r') as f:
-        osm_way_ids = [line.rstrip('\n') for line in f]
-    # remove the `w` in front of each id number if present
-    if osm_way_ids[0][0] == 'w':
-        osm_way_ids = [item.split('w')[1] for item in osm_way_ids]
-    # remove duplicates
-    osm_way_ids = list(set(osm_way_ids))
-
-    return osm_way_ids
+# def write_toll_ids(toll_ids, outpath):
+#     '''
+#     Given a list of way ids, write them to a file named 'toll_ids' where each is
+#     on a separate line. Such files can be used to subset a .osm.pbf with osmium
+#     e.g. `osmium getid -i toll_ids -r input.osm.pbf -o tolls.osm.pbf`
+#     :param toll_ids: list of way ids (str)
+#     :param outpath: path to destination folder
+#     :return: write a list of way ids e.g. `w123456` (str)
+#
+#     the reason we are keeping the 'w' while writing the ids, is that they are
+#     required by the osmium command detailed above
+#     '''
+#     with open(os.path.join(outpath, 'toll_ids'), 'w') as f:
+#         for item in toll_ids:
+#             temp = item
+#             f.write("%s\n" % temp)
 
 
-def extract_network_id_from_osm_id(network, osm_way_ids):
-    '''
-    Parse a Network() object and find edges whose
-    ['attributes']['osm:way:id']['text'] is present in osm_way_ids
-    :param network: a Network() object with 'osm:way:id'
-    :param osm_way_ids: a list of OSM way ids (without the `w` prefix) (str)
-    :return: a list of network edge ids (str)
-    '''
-
-    # a notion of progress bar could be nice but it's tricky to implement here
-    # as we don't know how long the loop will have to run for (see break clause)
-    network_toll_ids = []
-    edge_osm_ids = []
-
-    for link_id, link_attribs in network.links():
-        if 'attributes' in link_attribs.keys():
-            edge_osm_id = link_attribs['attributes']['osm:way:id']['text']
-            if type(edge_osm_id) is str:
-                if edge_osm_id in osm_way_ids:
-                    network_toll_ids.append(link_id)
-                    edge_osm_ids.append(edge_osm_id)
-            else:
-                continue
-
-        edge_osm_ids = list(set(edge_osm_ids))
-        if edge_osm_ids == osm_way_ids:
-            break
-
-    print(len(network_toll_ids), ' network ids identified as matching OSM toll ids')
-    print(len(osm_way_ids) - len(edge_osm_ids), ' OSM toll ids not found in network.xml')
-    missing_osm_ways = [item for item in osm_way_ids if item not in edge_osm_ids]
-    print('Unfound OSM ids are: ')
-    print(missing_osm_ways)
-
-    return network_toll_ids
+# def read_toll_ids(path):
+#     '''
+#     Read a list of OSM way ids
+#     :param path: path to folder
+#     :return: a list of OSM way ids (without the `w` prefix) (str)
+#     '''
+#     with open(path, 'r') as f:
+#         osm_way_ids = [line.rstrip('\n') for line in f]
+#     # remove the `w` in front of each id number if present
+#     if osm_way_ids[0][0] == 'w':
+#         osm_way_ids = [item.split('w')[1] for item in osm_way_ids]
+#     # remove duplicates
+#     osm_way_ids = list(set(osm_way_ids))
+#
+#     return osm_way_ids
 
 
 def extract_network_id_from_osm_csv(network, attribute_name, osm_csv_path, outpath):
@@ -126,7 +89,7 @@ def extract_network_id_from_osm_csv(network, attribute_name, osm_csv_path, outpa
     :param network: a Network() object with attribute_name tags
     :param attribute_name: a string corresponding to the name of the link attribute of interest
     :param osm_csv_path: path to a .csv config file where OSM way ids are stored in column `osm_ids`
-    :param outpath: path to a .csv file to be written
+    :param outpath: path to a folder
     :return: None, but will write .csv and .json files to `outpath` location
     '''
 
@@ -164,67 +127,10 @@ def extract_network_id_from_osm_csv(network, attribute_name, osm_csv_path, outpa
         # print unmatched ids
         print('these OSM way ids did not find a match in the network.xml')
         print(unmatched_osm_df['osm_ids'].values)
-        # # remove the corresponding rows from the dataframe
-        # print('these OSM way ids have been removed from the output saved at '+outpath)
-        # osm_df = osm_df.drop(osm_df[osm_df['network_id'].isnull()].index)
     # write dataframe as .csv and dictionary as .json
     osm_df.to_csv(os.path.join(outpath, 'osm_tolls_with_network_ids.csv'), index=False)
     with open(os.path.join(outpath, 'osm_to_network_ids.json'), 'w') as write_file:
         json.dump(osm_to_network_dict, write_file)
-
-
-# def extract_network_id_from_osm_csv(network, osm_csv_path, outpath):
-#     '''Parse a Network() object and find edges whose
-#     ['attributes']['osm:way:id']['text'] is present in a list of OSM way ids
-#     :param network: a Network() object with 'osm:way:id'
-#     :param osm_csv_path: path to a .csv config file where OSM way ids are stored in column `osm_ids`
-#     :param outpath: path to a .csv file to be written
-#     :return: None, but will write .csv file to `outpath` location
-#     '''
-#     osm_df = pd.read_csv(osm_csv_path, dtype=str)
-#     # for each row in osm_df, we want to add a values 'network_id'
-#     osm_df['network_id'] = pd.Series(dtype=str)
-
-#     edge_osm_ids = set()
-#     osm_ids = set(osm_df['osm_ids'].values)
-
-#     for link_id, link_attribs in network.links():
-#         if 'attributes' in link_attribs.keys():
-#             # extract the OSM id associated to that Network link
-#             # edge_osm_id = link_attribs['attributes']['osm:way:id']['text']
-#             edge_osm_id = link_attribs['attributes']['osm:osmid']['text']
-#             if str(edge_osm_id) in osm_ids:
-#                 # if the OSM id associated to that Network link is one of the OSM toll ways we are interested in
-#                 # then we extract the indices of all rows in the .csv where that OSM toll way appears
-#                 temp_index = osm_df[osm_df['osm_ids'] == edge_osm_id].index
-#                 if len(temp_index) > 1:
-#                     osm_df.loc[temp_index, 'network_id'] = link_id
-#                 else:
-#                     temp_index = temp_index[0]
-#                     osm_df.loc[temp_index, 'network_id'] = link_id
-
-#                 edge_osm_ids.update(edge_osm_id)
-#             else:
-#                 continue
-
-#         edge_osm_ids = set(edge_osm_ids)
-#         if edge_osm_ids == osm_ids:
-#             # if we have found all the OSM way ids, no need to go through the rest of the Network
-#             break
-#         # else:
-#         #     print(len(osm_ids) - len(edge_osm_ids), ' OSM ways still to be found.')
-
-#     # check whether some of our OSM ids were not found
-#     unmatched_osm_df = osm_df[osm_df['network_id'].isnull()]
-#     if unmatched_osm_df.shape[0] > 0:
-#         # print unmatched ids
-#         print('these OSM way ids did not find a match in the network.xml')
-#         print(unmatched_osm_df['osm_ids'].values)
-#         # remove the corresponding rows from the dataframe
-#         print('these OSM way ids have been removed from the output saved at '+outpath)
-#         osm_df = osm_df.drop(osm_df[osm_df['network_id'].isnull()].index)
-#     # write back to .csv
-#     osm_df.to_csv(outpath, index=False)
 
 
 def write_xml(root, path):
--- tests/test_road_pricing.py
+++ tests/test_road_pricing.py
@@ -1,8 +1,10 @@
 import os
 
+import json
 import lxml
-import pytest
 from lxml.etree import Element
+import pandas as pd
+import pytest
 
 from genet.core import Network
 from genet.use import road_pricing
@@ -21,41 +23,41 @@ def road_pricing_dtd():
 @pytest.fixture
 def road_pricing_sample_xml():
     sample_xml_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                   "test_data/road_pricing/sample_corden_road_pricing.xml"))
+                                                   "test_data/road_pricing/sample_cordon_road_pricing.xml"))
     yield lxml.etree.parse(sample_xml_path)
 
 
-def test_writes_toll_ids_to_expected_location(tmpdir):
-    toll_ids = [str(i) for i in range(10)]
-    road_pricing.write_toll_ids(toll_ids, tmpdir)
-
-    expected_toll_id_file = os.path.join(tmpdir, 'toll_ids')
-    assert os.path.exists(expected_toll_id_file)
-    with open(expected_toll_id_file) as toll_file:
-        lines = toll_file.readlines()
-        assert len(lines) == len(toll_ids)
-        for i, toll_id in enumerate(toll_ids):
-            assert lines[i] == "{}\n".format(toll_id)
-
-
-def test_read_toll_ids(path='tests/test_data/road_pricing/test_osm_toll_ids'):
-    toll_ids = road_pricing.read_toll_ids(path)
-    # check that returns a list
-    assert isinstance(toll_ids, list)
-    # check that all items in list are strings and don't start with 'w'
-    for item in toll_ids:
-        assert isinstance(item, str)
-        assert item[0] != 'w'
+# def test_writes_toll_ids_to_expected_location(tmpdir):
+#     toll_ids = [str(i) for i in range(10)]
+#     road_pricing.write_toll_ids(toll_ids, tmpdir)
+#
+#     expected_toll_id_file = os.path.join(tmpdir, 'toll_ids')
+#     assert os.path.exists(expected_toll_id_file)
+#     with open(expected_toll_id_file) as toll_file:
+#         lines = toll_file.readlines()
+#         assert len(lines) == len(toll_ids)
+#         for i, toll_id in enumerate(toll_ids):
+#             assert lines[i] == "{}\n".format(toll_id)
 
 
-def test_read_toll_ids_w_prefix(path='tests/test_data/road_pricing/test_osm_toll_ids_w_prefix'):
-    toll_ids = road_pricing.read_toll_ids(path)
-    # check that returns a list
-    assert isinstance(toll_ids, list)
-    # check that all items in list are strings and don't start with 'w'
-    for item in toll_ids:
-        assert isinstance(item, str)
-        assert item[0] != 'w'
+# def test_read_toll_ids(path='tests/test_data/road_pricing/test_osm_toll_ids'):
+#     toll_ids = road_pricing.read_toll_ids(path)
+#     # check that returns a list
+#     assert isinstance(toll_ids, list)
+#     # check that all items in list are strings and don't start with 'w'
+#     for item in toll_ids:
+#         assert isinstance(item, str)
+#         assert item[0] != 'w'
+#
+#
+# def test_read_toll_ids_w_prefix(path='tests/test_data/road_pricing/test_osm_toll_ids_w_prefix'):
+#     toll_ids = road_pricing.read_toll_ids(path)
+#     # check that returns a list
+#     assert isinstance(toll_ids, list)
+#     # check that all items in list are strings and don't start with 'w'
+#     for item in toll_ids:
+#         assert isinstance(item, str)
+#         assert item[0] != 'w'
 
 
 # def test_extract_toll_ways_from_opl(path_opl='tests/test_data/road_pricing/test.osm.opl'):
@@ -68,22 +70,83 @@ def test_read_toll_ids_w_prefix(path='tests/test_data/road_pricing/test_osm_toll
 #         assert item[0]=='w'
 
 
-def test_extract_network_id_from_osm_id(path_network='tests/test_data/road_pricing/network.xml',
-                                        path_osm_way_ids='tests/test_data/road_pricing/test_osm_toll_ids'):
+def test_extract_network_id_from_osm_csv(tmpdir,
+                                         path_network='tests/test_data/road_pricing/network.xml',
+                                         attribute_name = 'osm:way:id',
+                                         path_osm_csv = 'tests/test_data/road_pricing/osm_toll_id_ref.csv'
+                                         ):
     n = Network()
-    n.read_matsim_network(path_network, epsg='epsg:2157')
+    n.read_matsim_network(path_network, epsg='epsg:27700')
 
-    osm_way_ids = road_pricing.read_toll_ids(path_osm_way_ids)
+    road_pricing.extract_network_id_from_osm_csv(n,attribute_name, path_osm_csv, tmpdir)
 
-    toll_ids = road_pricing.extract_network_id_from_osm_id(n, osm_way_ids)
-    # check that returns a list
-    assert isinstance(toll_ids, list)
-    # check that the list is non-empty
-    assert len(toll_ids) > 0
-    # check that all items in list are strings
-    for item in toll_ids:
-        assert isinstance(item, str)
-    # do we need to test whether the contents of `toll_ids` are indeed part of network `n` ? YES
+    # check that returns .csv and .json files
+    expected_csv_path = os.path.join(tmpdir, 'osm_tolls_with_network_ids.csv')
+    expected_json_path = os.path.join(tmpdir, 'osm_to_network_ids.json')
+    assert os.path.exists(expected_csv_path)
+    assert os.path.exists(expected_json_path)
+    # check that the files are non-empty
+    expected_csv = pd.read_csv(expected_csv_path)
+    with open(expected_json_path, 'r') as f:
+        expected_json = json.load(f)
+    assert expected_csv.shape[0] > 0
+    assert (len(expected_json.keys()) > 0) and (len(expected_json.values()) > 0)
+    # check that the relevant column of the .csv contains the expected values
+    assert set(expected_csv['network_id'].unique()) == set(['yes', 'no'])
+    # check that the contents of the .json as expected
+    test_json_path = 'tests/test_data/road_pricing/osm_to_network_ids.json'
+    with open(test_json_path, 'r') as f:
+        test_json = json.load(f)
+    assert expected_json == test_json
+
+
+def test_builds_valid_xml_tree_from_csv_json(road_pricing_dtd,
+                                             path_csv='tests/test_data/road_pricing/osm_tolls_with_network_ids.csv',
+                                             path_json='tests/test_data/road_pricing/osm_to_network_ids.json',
+                                             ):
+
+    xml_tree = road_pricing.build_tree_from_csv_json(path_csv, path_json)
+    assert isinstance(xml_tree, lxml.etree._Element)
+    assert road_pricing_dtd.validate(xml_tree), \
+        'Tree generated at is not valid against DTD due to {}'.format(road_pricing_dtd.error_log.filter_from_errors())
+
+
+def test_builds_xml_tree_with_correct_content_from_csv_json(
+        path_csv='tests/test_data/road_pricing/osm_tolls_with_network_ids.csv',
+        path_json='tests/test_data/road_pricing/osm_to_network_ids.json'
+        ):
+
+    xml_tree_root = road_pricing.build_tree_from_csv_json(path_csv, path_json)
+
+    assert xml_tree_root.tag == 'roadpricing'
+    assert xml_tree_root.attrib == {'type': 'cordon', 'name': 'cordon-toll'}
+    assert len(xml_tree_root) == 2  # description, links
+
+    descs = xml_tree_root.findall('description')
+    assert len(descs) == 1
+    assert descs[0].tag == 'description'
+    assert descs[0].text == 'A simple cordon toll scheme'
+
+    costs = xml_tree_root.xpath('//cost')
+    assert len(costs) == 108
+    for cost in costs:
+        assert cost.tag == 'cost'
+        assert cost.attrib.keys() == ['start_time', 'end_time', 'amount']
+
+    links = xml_tree_root.findall('links')
+    assert len(links) == 1
+    assert links[0].tag == 'links'
+
+    with open(path_json, 'r') as f:
+        test_json = json.load(f)
+    all_network_ids = [item for sublist in test_json.values() for item in sublist]
+
+    assert len(xml_tree_root.xpath('//link')) == len(all_network_ids)
+    for link in xml_tree_root.xpath('//link'):
+        assert link.tag == 'link'
+        link_id = link.attrib['id']
+        all_network_ids.remove(link_id)
+    assert len(all_network_ids) == 0
 
 
 def test_writes_well_formed_and_valid_road_pricing_xml_file(tmpdir, road_pricing_dtd, road_pricing_sample_xml):
@@ -107,45 +170,6 @@ def test_writes_road_pricing_xml_file_with_expected_content(tmpdir, road_pricing
     assert_xml_trees_equal(road_pricing_sample_xml, xml_obj)
 
 
-def test_builds_valid_xml_tree_from_network_toll_ids(road_pricing_dtd):
-    toll_ids = [str(i) for i in range(10)]
-
-    xml_tree = road_pricing.build_tree(toll_ids)
-
-    assert isinstance(xml_tree, lxml.etree._Element)
-    assert road_pricing_dtd.validate(xml_tree), \
-        'Tree generated at is not valid against DTD due to {}'.format(road_pricing_dtd.error_log.filter_from_errors())
-
-
-def test_builds_xml_tree_with_correct_content_from_network_toll_ids():
-    toll_ids = [str(i) for i in range(10)]
-
-    xml_tree_root = road_pricing.build_tree(toll_ids)
-
-    assert xml_tree_root.tag == 'roadpricing'
-    assert xml_tree_root.attrib == {'type': 'cordon', 'name': 'cordon-toll'}
-    assert len(xml_tree_root) == 3 # description, links, cost subelements
-
-    descs = xml_tree_root.findall('description')
-    assert len(descs) == 1
-    assert descs[0].tag == 'description'
-    assert descs[0].text == 'A simple cordon toll scheme'
-
-    costs = xml_tree_root.findall('cost')
-    assert len(costs) == 1
-    assert costs[0].tag == 'cost'
-    assert costs[0].attrib == {'start_time': '00:00', 'end_time': '23:59', 'amount': '2.00'}
-
-    links = xml_tree_root.findall('links')
-    assert len(links) == 1
-    assert links[0].tag == 'links'
-
-    assert len(links[0]) == len(toll_ids)
-    for link in links[0]:
-        assert link.tag == 'link'
-        link_id = link.attrib['id']
-        toll_ids.remove(link_id)
-    assert len(toll_ids) == 0
 
 
 ###########################################################
