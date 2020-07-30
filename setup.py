126998e8f8d3aa6f66b45be7be819cc7763cb28a
KasiaKoz <kasia.kozlowska@arup.com>
2020-07-30 17:05:23 +0100

    fix flake8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-requirementPath="requirements.txt"
+requirementPath = "requirements.txt"
 install_requires = []
 if os.path.isfile(requirementPath):
     with open(requirementPath) as f:
