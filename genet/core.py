3f2c10ede4f3496299d10b1736e14397641a795f
KasiaKoz <kasia.kozlowska@arup.com>
2020-08-06 15:14:25 +0100

    fix flake8

--- genet/core.py
+++ genet/core.py
@@ -114,7 +114,13 @@ class Network:
         """
         fig, ax = self.plot_graph(show=False)
         return plot.plot_non_routed_schedule_graph(
-            nx.MultiDiGraph(self.schedule.graph()), 'network_schedule.png', ax=ax, show=show, save=save, output_dir=output_dir)
+            nx.MultiDiGraph(self.schedule.graph()),
+            'network_schedule.png',
+            ax=ax,
+            show=show,
+            save=save,
+            output_dir=output_dir
+        )
 
     def reproject(self, new_epsg):
         """
