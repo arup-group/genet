d33910f87f30ede59f5bb6c79adcbf5a01bd9cc8
KasiaKoz <kasia.kozlowska@arup.com>
2021-10-20 14:55:25 +0100

    replace plot functions

--- genet/utils/plot.py
+++ genet/utils/plot.py
@@ -1,10 +1,8 @@
-import os
 from copy import deepcopy
 from typing import Dict
 
 import geopandas as gpd
 import keplergl
-import osmnx as ox
 
 
 def plot_geodataframes_on_kepler_map(gdfs: Dict[str, gpd.GeoDataFrame], height=750, kepler_config=None):
@@ -14,49 +12,6 @@ def plot_geodataframes_on_kepler_map(gdfs: Dict[str, gpd.GeoDataFrame], height=7
         kepler_config = {}
     m = keplergl.KeplerGl(data=gdfs, height=height, config=kepler_config)
     return m
-
-
-def plot_graph(g, filename, show=True, save=False, output_dir='', e_c='#999999'):
-    if output_dir:
-        filepath = os.path.join(output_dir, filename + '.png')
-    else:
-        filepath = None
-    return ox.plot_graph(
-        g, filepath=filepath, node_size=2, edge_color=e_c, save=save, show=show)
-
-
-def plot_graph_routes(g, routes, filename, show=True, save=False, output_dir=''):
-    if output_dir:
-        filepath = os.path.join(output_dir, filename + '.png')
-    else:
-        filepath = None
-    if len(routes) == 0:
-        return ox.plot_graph(
-            g, filepath=filepath, node_size=2, save=save, show=show)
-    elif len(routes) == 1:
-        return ox.plot_graph_route(
-            g, route=routes[0], route_color="#F9A825", route_linewidth=1, orig_dest_size=5, node_size=2,
-            filepath=filepath, save=save, show=show)
-    else:
-        return ox.plot_graph_routes(
-            g, routes=routes, route_colors="#F9A825", route_linewidth=1, orig_dest_size=5, node_size=2,
-            filepath=filepath, save=save, show=show)
-
-
-def plot_non_routed_schedule_graph(g, filename, ax=None, show=True, save=False, output_dir=''):
-    if output_dir:
-        filepath = os.path.join(output_dir, filename + '.png')
-    else:
-        filepath = None
-    return ox.plot_graph(
-        g,
-        ax=ax,
-        filepath=filepath,
-        node_size=1,
-        edge_color='#F9A825',
-        node_color='#F9A825',
-        save=save,
-        show=show)
 
 
 KEPLER_CONFIGS = {
