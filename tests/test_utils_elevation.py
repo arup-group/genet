8130ab937a0159cfee5bd44c3a8de9ce6ca4a491
Anastasia Kopytina <65538698+ana-kop@users.noreply.github.com>
2022-06-23 12:03:58 +0100

    LAB-1717: Add functionality for adding slope attribute to links (#120)
    * add function for adding slope attribute to links
    
    * change the functions for getting node elevation and link slope to return dictionaries instead of adding attributes to network
    
    * replace distance by existing link length; fix script; add to-dos
    
    * addresses comments by Kasia
    
    * addresses comments from Michael
    
    * brings fixture inside the test

--- tests/test_utils_elevation.py
+++ tests/test_utils_elevation.py
@@ -2,12 +2,12 @@ import pytest
 import genet.utils.elevation as elevation
 import xarray as xr
 import os
+from tests.fixtures import assert_semantically_equal
 
 
 elevation_test_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "test_data", "elevation"))
 tif_path = os.path.join(elevation_test_folder, 'hk_elevation_example.tif')
 array_path = os.path.join(elevation_test_folder, 'elevation_image.nc')
-
 
 def test_output_type_get_elevation_image():
     img = elevation.get_elevation_image(tif_path)
@@ -25,3 +25,14 @@ def test_get_elevation_data():
     image = elevation.get_elevation_image(tif_path)
     output = elevation.get_elevation_data(image, 22.268764, 114.159062)
     assert output == 446
+
+
+def test_validation_report_for_node_elevation_dictionary():
+    # based on network4() fixture
+    elevation_dictionary = {'101982': {'z': -51}, '101990': {'z': 100}}
+    report = elevation.validation_report_for_node_elevation(elevation_dictionary)
+    correct_report = {'summary': {'extremely_high_values_count': 0, 'extremely_low_values_count': 1, 'max_value': 100,
+                                  'mean': 24, 'median': 24, 'min_value': -51, 'total_nodes': 2},
+                      'values': {'extremely_high_values_dict': {}, 'extremely_low_values_dict': {'101982': -51}}}
+
+    assert_semantically_equal(report, correct_report)
