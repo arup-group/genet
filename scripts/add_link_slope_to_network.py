7487e61d1d842a7020373d71dc64d313ef4cb8c3
Anastasia Kopytina <65538698+ana-kop@users.noreply.github.com>
2022-08-05 15:52:55 +0100

    fixes the add_link_slope_to_network.py script

--- scripts/add_link_slope_to_network.py
+++ scripts/add_link_slope_to_network.py
@@ -5,7 +5,7 @@ import json
 
 from genet import read_matsim
 from genet.utils.persistence import ensure_dir
-import genet.outputs_handler.sanitiser as sanitiser
+import genet.output.sanitiser as sanitiser
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description='Add elevation data to network nodes and validate it')
@@ -35,6 +35,11 @@ if __name__ == '__main__':
                             help='Output directory for the updated network',
                             required=True)
 
+    arg_parser.add_argument('-we',
+                            '--write_elevation_to_network',
+                            help='Whether node elevation data should be written as attribute to the network; defaults to True',
+                            default=True)
+
     arg_parser.add_argument('-sj',
                             '--save_jsons',
                             help='Whether elevation and slope dictionaries and report are saved; defaults to True',
@@ -46,6 +51,7 @@ if __name__ == '__main__':
     elevation = args['elevation']
     tif_null_value = args['null_value']
     output_dir = args['output_dir']
+    write_elevation_to_network = args['write_elevation_to_network']
     save_dict_to_json = args['save_jsons']
     elevation_output_dir = os.path.join(output_dir, 'elevation')
     ensure_dir(elevation_output_dir)
@@ -63,29 +69,41 @@ if __name__ == '__main__':
     elevation_dictionary = n.get_node_elevation_dictionary(elevation_tif_file_path=elevation, null_value=tif_null_value)
 
     if save_dict_to_json is True:
-        with open(os.path.join(elevation_output_dir, 'elevation/node_elevation_dictionary.json'), 'w',
+        with open(os.path.join(elevation_output_dir, 'node_elevation_dictionary.json'), 'w',
                   encoding='utf-8') as f:
             json.dump(sanitiser.sanitise_dictionary(elevation_dictionary), f, ensure_ascii=False, indent=4)
 
-    logging.info('Validating the elevation data added to network nodes')
-    report = elevation.validation_report_for_node_elevation(elevation_dictionary)
+
+    logging.info('Validating the node elevation data')
+    report = genet.utils.elevation.validation_report_for_node_elevation(elevation_dictionary)
     logging.info(report['summary'])
 
     if save_dict_to_json is True:
-        with open(os.path.join(elevation_output_dir, 'elevation/validation_report_for_elevation.json'), 'w',
+        with open(os.path.join(elevation_output_dir, 'validation_report_for_elevation.json'), 'w',
                   encoding='utf-8') as f:
             json.dump(sanitiser.sanitise_dictionary(report), f, ensure_ascii=False, indent=4)
+
+
+    if write_elevation_to_network is True:
+        logging.info('Adding node elevation as attribute to the network')
+        node_attrib_dict = {}
+        for node_id in elevation_dictionary.keys():
+            elevation_value = elevation_dictionary[node_id]['z']
+            node_attrib_dict[node_id] = {'z': elevation_value}
+
+        n.apply_attributes_to_nodes(node_attrib_dict)
+
 
     logging.info('Creating slope dictionary for network links')
     slope_dictionary = n.get_link_slope_dictionary(elevation_dict=elevation_dictionary)
 
     if save_dict_to_json is True:
-        with open(os.path.join(elevation_output_dir, 'elevation/link_slope_dictionary.json'), 'w',
+        with open(os.path.join(elevation_output_dir, 'link_slope_dictionary.json'), 'w',
                   encoding='utf-8') as f:
-            json.dump(sanitiser.sanitise_dictionary(report), f, ensure_ascii=False, indent=4)
+            json.dump(sanitiser.sanitise_dictionary(slope_dictionary), f, ensure_ascii=False, indent=4)
 
-    logging.info('Adding link slope as attribute to the network')
 
+    logging.info('Adding link slope as an additional attribute to the network')
     attrib_dict = {}
     for link_id in slope_dictionary.keys():
         slope_value = slope_dictionary[link_id]['slope']
@@ -93,6 +111,7 @@ if __name__ == '__main__':
             'attributes': {'slope': {'name': 'slope', 'class': 'java.lang.String', 'text': slope_value}}}
 
     n.apply_attributes_to_links(attrib_dict)
+
 
     logging.info('Writing the updated network')
     n.write_to_matsim(elevation_output_dir)
