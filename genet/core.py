36461f60cb28d2670d90a1dc8a24ce0d737e44a6
Anastasia Kopytina <65538698+ana-kop@users.noreply.github.com>
2022-08-19 12:36:43 +0100

    get rid of summary report in standard outputs for now

--- genet/core.py
+++ genet/core.py
@@ -37,7 +37,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 
 class Network:
-    def __init__(self, epsg, **kwargs) -> object:
+    def __init__(self, epsg, **kwargs):
         self.epsg = epsg
         self.transformer = Transformer.from_crs(epsg, 'epsg:4326', always_xy=True)
         self.graph = nx.MultiDiGraph(name='Network graph', crs=epsg)
